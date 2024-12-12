cb2d434b7e1042762a6bc49a31e07b99b15f6de4
Pranav J S <jspranav15@gmail.com>
2024-12-12 12:40:36 +0530

    Remove unrelated folders and isolate captain service

--- ride/routes/ride.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middleware/auth.middleware");
-const rideController = require("../controller/ride.controller");
-
-router.post("/create-ride", authMiddleware.userAuth, rideController.createRide);
-
-module.exports = router;
